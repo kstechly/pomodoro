056bea348ec5e1a4b7880b48e663f809f58f0628
kstechly <kayastechly@gmail.com>
2022-04-06 13:21:11 -0700

    now logs pomos with pomodoro-server

--- pomodoro.py
+++ pomodoro.py
@@ -5,8 +5,11 @@ import time
 import winsound
 import json
 from os import path
+import requests
 
 data = []
+api_url = "http://localhost:5000"
+
 
 if path.isfile('log.json') is False:
     with open('log.json','w') as f:
@@ -26,19 +29,22 @@ def pomo_end():
     print("Project?")
     proj = input()
     numprev = 0
-    get_data()
-    add_data(timestamp,proj,cat)
+    data = get_data()
+    add_data(data,timestamp,proj,cat)
     numprev = len([x for x in data if x["Category"]==cat])
     print("Logged. You've now completed "+str(numprev)+" pomodoros in this category.")
 
 def get_data():
-    with open("log.json") as f:
-        data = json.load(f)
+    #with open("log.json") as f:
+    #    return json.load(f)
+    return requests.get(api_url+"/get").json()
 
-def add_data(timestamp,proj,cat):
-    data.append({"Timestamp": str(timestamp), "Project": proj, "Category": cat})
-    with open("log.json",'w') as f:
-        json.dump(data, f)
+def add_data(data, timestamp,proj,cat):
+    #data.append({"Timestamp": str(timestamp), "Project": proj, "Category": cat})
+    #with open("log.json",'w') as f:
+    #    json.dump(data, f)
+    query = {'Timestamp':timestamp, 'Project':proj,'Category':cat}
+    response = requests.get(api_url+"/post", params=query)
 
 def start_countdown(minutes):
     minute_ctd(minutes*60)
